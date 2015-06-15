a3d74d18f4e38223e099ffa8348675f0eef1a117
joeyism <sham.joey@gmail.com>
2015-06-15 19:09:12 -0400

    updated event emitter

--- index.js
+++ index.js
@@ -2,19 +2,22 @@
 'use strict';
 var commit = require('commit-cli');
 var git = require('./lib/git');
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
 require('colors');
 
-git.getCurrentBranch().then(function(branch){
-    
-    return git.push(branch);
-    
-}).then(function(branch){
+eventEmitter.on('endCommit', function(){
+    git.getCurrentBranch().then(function(branch){
 
-    console.log('Push to branch ' + branch.bold + 'successfully');
-    
-}).catch(function(err){
+        return git.push(branch);
 
-    console.log(err.toString().red);
+    }).then(function(branch){
 
+        console.log('Push to branch ' + branch.bold + 'successfully');
+
+    }).catch(function(err){
+
+        console.log(err.toString().red);
+
+    });
 });
-
